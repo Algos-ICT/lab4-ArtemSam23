b2dc074072d4d84b39ea46e4886b9fa0456827c5
ArtemSam23 <artemsam23@gmail.com>
2021-11-13 14:00:20 +0300

    done the 3d task

---
+++ code files/task_03.py
@@ -0,0 +1,29 @@
+from collections_for_lab4 import Stack
+
+
+f = open('input_03.txt', 'r')
+output = open('output_03.txt', 'x')
+n = int(f.readline())
+for i in range(n):
+    s = f.readline()
+    stack = Stack()
+    for j in s:
+        if j in '([':
+            stack.push(j)
+        elif not stack.is_empty():
+            if j == ')':
+                if stack.top.value == '(':
+                    stack.pop()
+                else:
+                    stack.push(j)
+            elif j == ']':
+                if stack.top.value == '[':
+                    stack.pop()
+                else:
+                    stack.push(j)
+        else:
+            stack.push()
+    if stack.is_empty():
+        print('YES', file=output)
+    else:
+        print('NO', file=output)
